a98b0a356fac627e714d69db59b7a834ea0d3c1a
Dr.n0psl3d <41803496+DrN0psl3d@users.noreply.github.com>
2019-01-26 21:12:57 +0100

    Create vpnchecker.sh

---
+++ vpn/vpnchecker.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Get vpnclient interfaces
+VPNCLIENTS=`ifconfig | grep 'ovpnc' | grep -v 'inet' | cut -f1 -d ':'`
+
+for CLIENT in $VPNCLIENTS
+do
+	DATE=`date +%d-%m-%Y-%T`
+	VPNGATEWAY=`/sbin/ifconfig $CLIENT | grep 'inet ' | cut -f1 -d '-' | awk '{ print $2}'`
+	echo "$DATE : VPN Client [$CLIENT] address: $VPNGATEWAY" >> vpncheck.log
+	if [ -z $VPNGATEWAY ] # if this is empty... the vpn is down
+	then
+		DATE=`date +%d-%m-%Y-%T`
+		echo "$DATE : VPN Client [$CLIENT] is down, restarting..." >> vpncheck.log
+		CLIENTNUM=`echo $CLIENT | cut -c 6`
+		/usr/local/sbin/pfSsh.php playback svc restart openvpn client $CLIENTNUM
+	else
+		DATE=`date +%d-%m-%Y-%T`
+		echo "$DATE : VPN Client [$CLIENT] is up... doing nothing..." >> vpncheck.log
+	fi
+done
